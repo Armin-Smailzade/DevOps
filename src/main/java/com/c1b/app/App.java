11cb2374142de2b4af0fb52fa433000052c50ed7
armin esmaeilzadeh <armin.esmaeilzadeh@gmail.com>
2019-04-28 14:43:00 -0700

    initial files

---
+++ src/main/java/com/c1b/app/App.java
@@ -0,0 +1,17 @@
+package com.c1b.app;
+
+import java.util.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main( String[] args ){
+
+        Deque<String> testQueue = new LinkedList<String>();
+
+        System.out.println( "Test" );
+    }
+}
